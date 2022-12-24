370fba43552e1d7bfd10aa956c91c320e0f783b5
rqpt <1ernstvermeulen1@gmail.com>
2022-12-24 22:32:12 +0200

    scrolloff adjust

--- lua/rqpt/remap.lua
+++ lua/rqpt/remap.lua
@@ -1,9 +1,11 @@
+-- kry die leader p copy paste map
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>op", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
